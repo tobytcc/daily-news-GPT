e586b8598a335a6f0feacc8f571bd87feac33ae8
Kevin Shen <kevinss1227@gmail.com>
2023-09-24 00:47:09 -0700

    added black line-length checker to pre-commit

--- cnn_scraper.py
+++ cnn_scraper.py
@@ -1,12 +1,12 @@
 from cnn_scrape_helper import get_top_news_link, get_article_text, HTTPConnectionError
 
 try:
-    top_news_links = get_top_news_link('us', limit = 3)
+    top_news_links = get_top_news_link("us", limit=3)
     for news_item in top_news_links:
-        print(news_item['path'])
+        print(news_item["path"])
 
     if top_news_links:
-        article_path = top_news_links[0]['path']
+        article_path = top_news_links[0]["path"]
         paragraphs = get_article_text(article_path)
 
     print(paragraphs)
