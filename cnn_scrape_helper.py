19f9ded7773a7c4043ddf8c3d6eabd42ceedb866
Kevin Shen <kevinss1227@gmail.com>
2023-09-24 00:35:42 -0700

    enable ability to extract paragraphs from article, given path

--- cnn_scrape_helper.py
+++ cnn_scrape_helper.py
@@ -6,41 +6,87 @@ import yaml
 class HTTPConnectionError(Exception):
     pass
 
-def get_top_news_link(category: str, limit: int=3) -> list[tuple[str, str]]:
+def get_top_news_link(category: str, limit: int=3) -> list[dict[str, str]]:
+    """
+    returns a list of top headlines given the desired category
+
+    category: category of news to to scrape headlines from
+    limit: limit of headlines, default 3
+
+    the scraping logic is that it seems like the extract is a headline
+    iff the attr data-editable is headline, and its 3rd level parent is
+    a hyperlink reference
+    """
+
     with open('cnn_config.yaml', 'r') as config_file:
         cnn_config = yaml.safe_load(config_file)
 
     section_details = cnn_config['sections'][category]
-    URL = section_details['url']
+    url = section_details['url']
 
-    headline_list: list[tuple[str, str]] = []
+    headline_list: list[dict[str, str]] = []
 
-    response = requests.get(URL)
+    response = requests.get(url)
     if response.status_code == 200:
-        print('successfully connected to', URL)
+        print('successfully connected to', url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        extracted_headlines = soup.find_all(attrs=section_details['attrs'])
+        extracts = soup.find_all(attrs=section_details['attrs'])
 
         headlines_obtained = 0
-        for headline in extracted_headlines:
+        for extract in extracts:
             if headlines_obtained >= limit:
                 return headline_list
             #all headlines seem to have href attribute as its 3rd level parents
-            parent_1 = headline.find_parent()
+            parent_1 = extract.find_parent()
             parent_2 = parent_1.find_parent() if parent_1 else None
             parent_3 = parent_2.find_parent() if parent_2 else None
 
             if parent_3 and parent_3.name == 'a' and 'href' in parent_3.attrs:
                 href = parent_3.attrs['href']
-                headline_list.append((headline.text.strip(), href))
+                headline_list.append({'title': extract.text.strip(), 'path': href})
 
                 headlines_obtained += 1
-        return headline_list
 
     else:
         raise HTTPConnectionError(
-            f"Failed to connect to {URL}. Status code: {response.status_code}"
+            f"Failed to connect to {url}. Status code: {response.status_code}"
         )
 
-    return []
+    return headline_list
+
+
+def get_article_text(path: str) -> list[str]:
+    """
+    given a hyperlink to a cnn article, return the paragraphs as list.
+
+    path: path to append to domain https://www.cnn.com/ to access article
+
+    the scraping logic is that it seems like all pargraphs from the articles come
+    from paragraph attributes with parent of class 'article__cnontent'.
+    """
+    with open('cnn_config.yaml', 'r') as config_file:
+        cnn_config = yaml.safe_load(config_file)
+
+    base_url = cnn_config['base_url']
+    url = base_url + path
+
+    article_config = cnn_config['article']
+
+    paragraph_list: list[str] = []
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        print('successfully connected to', url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        article_content = soup.find(name='div', attrs=article_config['attrs'])
+
+        paragraph_list = [p_obj.text.strip() for p_obj in article_content.find_all(name = 'p')]
+
+    else:
+        raise HTTPConnectionError(
+            f"Failed to connect to {url}. Status code: {response.status_code}"
+        )
+
+    return paragraph_list
--- cnn_scraper.py
+++ cnn_scraper.py
@@ -1,9 +1,15 @@
-from cnn_scrape_helper import get_top_news_link, HTTPConnectionError
+from cnn_scrape_helper import get_top_news_link, get_article_text, HTTPConnectionError
 
 try:
-    top_news_links = get_top_news_link('US')
-except HTTPConnectionError as e:
-    print(e)
+    top_news_links = get_top_news_link('us', limit = 3)
+    for news_item in top_news_links:
+        print(news_item['path'])
 
-for news in top_news_links:
-    print(news)
+    if top_news_links:
+        article_path = top_news_links[0]['path']
+        paragraphs = get_article_text(article_path)
+
+    print(paragraphs)
+
+except HTTPConnectionError as error:
+    print(error)
