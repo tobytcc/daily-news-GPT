512655da904225dd78c75dcff201a6b5b979eaae
Kevin Shen <kevinss1227@gmail.com>
2023-09-27 00:54:02 -0700

    finished article text scrape logic for bbc scraper

--- scrapers/bbc_scraper.py
+++ scrapers/bbc_scraper.py
@@ -3,10 +3,14 @@ this module contains helper functions to scrape BBC
 """
 
 from typing import Union
+from urllib.parse import urljoin
+from bs4 import Tag
 
 import yaml
 
 from scrapers.scrape_helper import make_request
+
+from models.data_models import Article
 
 CONFIG_FILE = "config/bbc_config.yaml"
 
@@ -45,9 +49,64 @@ def get_top_news(cat: str, subcat: Union[str, None], limit: int = 3) -> list[dic
         if parent and parent.name == "a" and "href" in parent.attrs:
             href = parent.attrs["href"]
 
+            if "live" in href.split(
+                "/"
+            ):  # filter out live article, as I don't know how to scrape it
+                continue
+
             headline = {"title": extract.text.strip(), "path": href}
             if headline not in headline_list:
                 headline_list.append(headline)
                 headlines_obtained += 1
 
     return headline_list
+
+
+def get_article_text(path: str) -> list[str]:
+    """
+    given a hyperlink to a bbc article, return the paragraphs as list.
+
+    path: path to append to domain https://www.bbc.com/ to access article
+    """
+    with open(CONFIG_FILE, "r", encoding="UTF-8") as config_file:
+        bbc_config = yaml.safe_load(config_file)
+
+    base_url = bbc_config["base_url"]
+    url = urljoin(base_url, path)
+
+    article_config = bbc_config["article"]
+
+    paragraph_list: list[str] = []
+
+    soup = make_request(url)
+
+    article_content = soup.find(name="article")
+
+    if isinstance(article_content, Tag):
+        article_text_blocks = article_content.find_all(name="div", attrs=article_config["attrs"])
+
+        for text_block in article_text_blocks:
+            if isinstance(text_block, Tag):
+                text_obj = text_block.find(name="p")
+                if isinstance(text_obj, Tag):
+                    paragraph_list.append(text_obj.text.strip())
+
+    return paragraph_list
+
+
+def get_articles(cat: str, subcat: Union[str, None], limit: int = 3) -> list[Article]:
+    """
+    returns a list of Article objects storing path, title, and paragraph texts from the
+    given category, with length no more than limit.
+
+    category: category within cnn to scrape articles from
+    limit: the maximum number of articles to scrape
+    """
+    article_list = []
+
+    top_news_list = get_top_news(cat, subcat, limit)
+    for news in top_news_list:
+        paragraphs = get_article_text(news["path"])
+        article_list.append(Article(path=news["path"], title=news["title"], text=paragraphs))
+
+    return article_list
